20cac799a2ad42b241b38a7e05b4dde86894c0ab
latifa.farid <latefamohamed137@gmail.com>
2023-12-08 13:37:50 +0200

    init

--- src/session_01/package1/ClassA.java
+++ src/session_01/package1/ClassA.java
@@ -1,4 +1,47 @@
 package session_01.package1;
 
+import java.util.Scanner;
+
 public class ClassA {
+    public static void main(String[] args) {
+       /* Scanner scanner = new Scanner(System.in);
+        System.out.println("enter number ");
+        try {
+            int number = scanner.nextInt();
+            if (number < 5) {
+                System.out.println("number is less than 5");
+            } else if (number > 5) {
+                System.out.println("number is more than 5");
+
+            } else {
+                System.out.println("number eque6l 5");
+            }
+
+
+    }
+        catch (Exception e){
+            System.out.println("enter valid input");
+        }
+    }*/
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enetr password ");
+        String password = scanner.nextLine();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("renetr password ");
+            String repassword = scanner.nextLine();
+            if (repassword.length()<3){
+                System.out.println("password less than 3 cr");
+                System.out.println("you have attempts:" + i + "out of 3");
+            }
+           /* if (password.equals(repassword)) {
+                System.out.println("true");
+                break;
+            } else {
+                System.out.println("you have attempts:" + i + "out of 3");
+            }*/
+        }
+    }
 }
--- src/session_01/package2/ClassC.java
+++ src/session_01/package2/ClassC.java
@@ -1,4 +1,16 @@
 package session_01.package2;
 
 public class ClassC {
+    public static void main(String[] args) {
+            String nameArraylist [];
+        nameArraylist =new String[4];
+        nameArraylist[0]="aya";
+        nameArraylist[1]="sara";
+        nameArraylist[2]="mai";
+        nameArraylist[3]="latifa";
+        for (int i=0 ;i<=3;i++){
+            System.out.println(nameArraylist[i]);
+        }
+
+    }
 }
