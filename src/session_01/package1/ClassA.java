b025fd811c16ee8a26de5eb68526bc7a5636d3d3
lobna <elqattanlobna4@gmail.com>
2023-12-08 13:51:36 +0200

    init

--- src/session_01/package1/ClassA.java
+++ src/session_01/package1/ClassA.java
@@ -1,4 +1,25 @@
 package session_01.package1;
 
+import java.util.Scanner;
+
 public class ClassA {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter number ");
+        try {
+            int number = scanner.nextInt();
+            if (number < 5) {
+                System.out.println("number is less than five");
+            } else if (number > 5) {
+                System.out.println("number is bigger than five");
+
+            } else {
+                System.out.println("number is 5");
+            }
+        } catch (Exception e) {
+            System.out.println("incorrect input");
+
+
+        }
+    }
 }
--- src/session_01/package2/ClassC.java
+++ src/session_01/package2/ClassC.java
@@ -1,4 +1,17 @@
 package session_01.package2;
 
 public class ClassC {
+    public static void main(String[] args) {
+        String nameArraylist[];
+        nameArraylist= new String[4];
+        nameArraylist[0]="l";
+        nameArraylist[1]="o";
+        nameArraylist[2]="b";
+        nameArraylist[3]="n";
+        for (int i=0;i<=3;i++){
+            System.out.println(nameArraylist[i]);
+        }
+
+
+    }
 }
